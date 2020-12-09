390e438e3d2343b06a3a647b1caf2e7210b2c311
Lauren-Vanderklok <54608582+Lauren-Vanderklok@users.noreply.github.com>
2020-12-08 23:03:09 -0500

    Update project.sql

--- project.sql
+++ project.sql
@@ -1,6 +1,6 @@
 SPOOL project.txt
 SET ECHO ON
-
+ 
 /*
 CIS 353-Database Design Project
 Tim Bomers
@@ -8,66 +8,65 @@ Lauren Freeman
 Jacob Lakies
 Lauren Vanderklok (Coordinator)
 */
-
+ 
 CREATE TABLE Books(
     ISBN INTEGER,
     title VARCHAR2(200), 
     genre VARCHAR2(100),
-    --apparantly the usual char datatype I've been using 
-    --MUST have that num of chars and this datatype does not have this problem
-
+  
+ 
     CONSTRAINT bIC1 PRIMARY KEY (ISBN)
 );
-
+ 
 CREATE TABLE Locations(
     BranchID INTEGER PRIMARY KEY,
     Name CHAR(100),
     Address CHAR(500)
 );
-
+ 
 CREATE TABLE CopyOfBook(
     Barcode INTEGER PRIMARY KEY,
     BranchID INTEGER,
     ISBN INTEGER,
-
+ 
     CONSTRAINT cIC1 FOREIGN KEY (ISBN) REFERENCES Books (ISBN)
         ON DELETE CASCADE,
     CONSTRAINT cIC2 FOREIGN KEY (BranchID) REFERENCES Locations (BranchID)
         ON DELETE SET NULL
 );
-
+ 
 CREATE TABLE Staff(
     StaffID INTEGER,
     Name CHAR(100),
     MentorID INTEGER UNIQUE,
     BranchID INTEGER,
-
+ 
     CONSTRAINT sIC1 PRIMARY KEY (StaffID),
-
+ 
     CONSTRAINT sIC2 FOREIGN KEY (MentorID) REFERENCES Staff (MentorID)
         ON DELETE SET NULL,
     
     CONSTRAINT sIC3 FOREIGN KEY (BranchID) REFERENCES Locations (BranchID)
         ON DELETE SET NULL
 );
-
+ 
 CREATE TABLE Authors(
     ISBN INTEGER,
     Author CHAR(100),
-
+ 
     CONSTRAINT aIC1 PRIMARY KEY (ISBN, Author),
     CONSTRAINT aIC2 FOREIGN KEY (ISBN) REFERENCES Books (ISBN)
         ON DELETE CASCADE
 );
-
+ 
 CREATE TABLE Patrons(
     PatronID INTEGER,
     Name CHAR(100),
     Address CHAR(500),
-
+ 
     CONSTRAINT pIC1 PRIMARY KEY (PatronID)
 );
-
+ 
 CREATE TABLE Transactions(
     PatronID INTEGER,
     Barcode INTEGER,
@@ -76,35 +75,43 @@ CREATE TABLE Transactions(
     ReturnDate DATE,
     DatePaid DATE,
     FeeAmount DECIMAL(6,2),
-
+ 
     CONSTRAINT tIC1 PRIMARY KEY (PatronID, Barcode, LoanDate),
     CONSTRAINT tIC2 CHECK (DueDate = LoanDate+14),
     CONSTRAINT tIC3 FOREIGN KEY (PatronID) REFERENCES Patrons (PatronID)
         ON DELETE CASCADE,
     CONSTRAINT tIC4 FOREIGN KEY (Barcode) REFERENCES CopyOfBook (Barcode)
-        ON DELETE SET NULL
+        ON DELETE SET NULL,
     CONSTRAINT tIC5 CHECK (FeeAmount <=100)
 );
-
+ 
 CREATE TABLE Supplier(
     SupplierID INTEGER PRIMARY KEY
 );
-
+ 
 CREATE TABLE Supplies(
     SupplierID INTEGER,
     ISBN INTEGER,
     Price DECIMAL(6,2),
-
+ 
     CONSTRAINT suppliesIC1 PRIMARY KEY (SupplierID, ISBN),
     CONSTRAINT suppliesIC2 FOREIGN KEY (SupplierID) REFERENCES Supplier (SupplierID)
         ON DELETE CASCADE,
     CONSTRAINT suppliesIC3 FOREIGN KEY (ISBN) REFERENCES Books (ISBN)
         ON DELETE CASCADE
 );
-
+ 
 --
 SET FEEDBACK OFF
-
+ 
+INSERT INTO Locations
+VALUES(1, 'Woodland', 'Reading Rd');
+INSERT INTO Locations
+VALUES(2, 'Rivertown', 'Learning Ln');
+INSERT INTO Locations
+VALUES(3, 'Farmington', 'Punctuation Pl');
+ 
+ 
 INSERT INTO Staff
 VALUES(731, 'Alice Brown', 733, 1);
 INSERT INTO Staff
@@ -123,14 +130,8 @@ INSERT INTO Staff
 VALUES(738, 'Chad Allard', 736, 3);
 INSERT INTO Staff
 VALUES(739, 'Levi Brand', 732, 2);
-
-INSERT INTO Locations
-VALUES(1, 'Woodland', 'Reading Rd');
-INSERT INTO Locations
-VALUES(2, 'Rivertown', 'Learning Ln');
-INSERT INTO Locations
-VALUES(3, 'Farmington', 'Punctuation Pl');
-
+ 
+ 
 INSERT INTO Books
 VALUES(0000001, 'SQL For Dummies', 'Textbook');
 INSERT INTO Books
@@ -145,7 +146,7 @@ INSERT INTO Books
 VALUES(0000006, 'SQL For World Domination', 'Geopolitical Theory');
 INSERT INTO Books
 VALUES(9999999, 'What To Do Once You Have Conquered The World Through SQL', 'Geopolitical Theory');
-
+ 
 INSERT INTO CopyOfBook
 VALUES(00000, '1', 0000002);
 INSERT INTO CopyOfBook
@@ -174,7 +175,7 @@ INSERT INTO CopyOfBook
 VALUES(01100, '3', 0000006);
 INSERT INTO CopyOfBook
 VALUES(01101, '3', 9999999);
-
+ 
 INSERT INTO Authors
 VALUES(0000001,'Paige Turner');
 INSERT INTO Authors
@@ -191,7 +192,7 @@ INSERT INTO Authors
 VALUES(0000006,'Dr. Arch');
 INSERT INTO Authors
 VALUES(9999999,'Kim Jong-Un');
-
+ 
 INSERT INTO Patrons
 VALUES(111, 'Emily Julian', '123 Parkway');
 INSERT INTO Patrons
@@ -204,29 +205,33 @@ INSERT INTO Patrons
 VALUES(555, 'Norman Gates', '231 Pleasant St');
 INSERT INTO Patrons
 VALUES(666, 'Alex DeLeon', '55321 Marina Pl');
-
+INSERT INTO Patrons
+VALUES(777, 'Alice Brown', '556 Road Rd');
+INSERT INTO Patrons
+VALUES(888, 'Dick Chaney', '1 Lonely Rd');
+ 
 INSERT INTO Transactions
-VALUES(111, 00010, 2008-10-03, 2008-10-17, 2008-10-02, NULL, NULL);
+VALUES(111, 00010, DATE '2008-10-03', DATE '2008-10-17', DATE '2008-10-02', NULL, NULL);
 INSERT INTO Transactions
-VALUES(222, 00111, 2010-03-21, 2010-04-05, 2010-03-27, 2010-03-27, 10.00);
+VALUES(222, 00111, DATE '2010-03-21', DATE '2010-04-04', DATE '2010-03-27', DATE '2010-03-27', 10.00);
 INSERT INTO Transactions
-VALUES(222, 00110, 2012-04-20, 2012-05-04, 2012-06-20, 2012-06-20, 62.00);
+VALUES(222, 00110, DATE '2012-04-20', DATE '2012-05-04', DATE '2012-06-20', DATE '2012-06-20', 62.00);
 INSERT INTO Transactions
-VALUES(444, 00001, 2016-12-22, 2016-12-22, 2017-01-06, NULL, NULL);
+VALUES(444, 00001, DATE '2016-12-8', DATE '2016-12-22', DATE '2017-01-06', NULL, NULL);
 INSERT INTO Transactions
-VALUES(333, 01010, 2020-05-14, 2020-05-28, 2020-06-17, NULL, 28.00);
+VALUES(333, 01010, DATE '2020-05-14', DATE '2020-05-28', DATE '2020-06-17', NULL, 28.00);
 INSERT INTO Transactions
-VALUES(555, 00011, 2020-03-18, 2020-04-01, 2020-04-22, 2020-05-20, 42.00);
+VALUES(555, 00011, DATE '2020-03-18', DATE '2020-04-01', DATE '2020-04-22', DATE '2020-05-20', 42.00);
 INSERT INTO Transactions
-VALUES(666, 01101, 2020-02-22, 2020-03-07, NULL, NULL, 100.00);
-
+VALUES(666, 01101, DATE '2020-02-22', DATE '2020-03-07', NULL, NULL, 100.00);
+ 
 INSERT INTO Supplier
 VALUES(121);
 INSERT INTO Supplier
 VALUES(122);
 INSERT INTO Supplier
 VALUES(123);
-
+ 
 INSERT INTO Supplies
 VALUES(121, 0000004, 4.95);
 INSERT INTO Supplies
@@ -234,11 +239,11 @@ VALUES(121, 0000003, 7.95);
 INSERT INTO Supplies
 VALUES(121, 0000002, 25.00);
 INSERT INTO Supplies
-VALUES(121, 0000002, 22.95);
+VALUES(121, 0000005, 22.95);
 INSERT INTO Supplies
 VALUES(121, 9999999, 299.99);
-
-
+ 
+ 
 SET FEEDBACK ON
 COMMIT;
 --
@@ -253,7 +258,7 @@ SELECT * FROM Supplier;
 SELECT * FROM Supplies;
 --
 -- queries, on of each type listed in project assignment doc
-
+ 
 --Join involving at least 4 relations
 -- the titles and author of every book at the location with BranchID = 3
 SELECT L.BranchID, C.ISBN, B.Title, A.Author 
@@ -270,65 +275,64 @@ FROM   Staff S, Staff M
 WHERE  M.StaffID = S.MentorID AND 
        S.BranchID != M.BranchID
 ORDER BY S.StaffID;
-
--- Division query, and query using MINUS
+ 
+-- Division query, Join four tables, and query using MINUS
 -- every patron who has checked out every book by author "Dr. Arch" 
 SELECT P.Name, P.PatronID
 FROM Patrons P
 WHERE NOT EXISTS(
     (SELECT A.ISBN 
     FROM Authors A
-    WHERE A.Author = "Dr. Arch")
+    WHERE A.Author = 'Dr. Arch')
     MINUS
     (SELECT A.ISBN
     FROM Transactions T, CopyOfBook C, Authors A
     WHERE P.PatronID = T.PatronID AND
         T.Barcode = C.Barcode AND
         C.ISBN = A.ISBN AND
-        A.Author = "Dr. Arch"
+        A.Author = 'Dr. Arch'
     )
 );
-
+ 
 --Query with MAX
 --The greatest amount owed by a patron
 SELECT MAX(FeeAmount) AS maxFee
 FROM Transactions;
-
+ 
 --GROUP BY, HAVING, and ORDER BY query
 --BranchID and number of workers for each branch with less than 3 workers
 SELECT S.BranchID, COUNT(*)
-FROM   Locations L, Staff S
-WHERE  S.BranchID = L.BranchID
-GROUP BY S.StaffID, S.BranchID
+FROM   Staff S
+GROUP BY S.BranchID
 HAVING COUNT(*) < 3
 ORDER BY S.BranchID;
-
+ 
+ 
 --Correlated subquery
 -- Find second greatest fee amount
-SELECT e1.Name, e1.FeeAmount
-FROM Transactions e1
-WHERE N-1 = 
-    (SELECT COUNT(DISTINCT FeeAmount)
-     FROM Transactions e2
-    WHERE e2.FeeAmount > e1.FeeAmount)
-ORDER BY e1.Name;
-
+SELECT PatronID
+FROM Transactions e
+WHERE 2=(SELECT COUNT(DISTINCT FeeAmount) FROM Transactions p
+WHERE e.FeeAmount<=p.FeeAmount);
+ 
+ 
 --Noncorrelated subquery
 --Find patrons who haven't owed any fees
-SELECT P.Name, P.PatronID
-FROM Patrons P
-WHERE P.PatronID NOT IN 
-    (SELECT T.PatronID FROM Transactions T
-    WHERE T.FeeAmount NOT NULL)
-ORDER BY P.PatronID;
-
+SELECT T.PatronID
+FROM Transactions T
+WHERE T.PatronID NOT IN 
+    (SELECT T2.PatronID  FROM Transactions T2
+    WHERE T2.FeeAmount IS NOT NULL)
+ORDER BY T.PatronID;
+ 
 --Outer join query
---Name of staff who also are patrons
-SELECT Staff.Name
-FROM Staff
-    FULL OUTER JOIN Patrons ON Staff.Name = Patrons.Name;
-    --may need "WHERE"
-
+--List patrons and whether or not they have any transactions
+SELECT Patrons.PatronID, Patrons.Name, Transactions.PatronID
+FROM Patrons LEFT OUTER JOIN Transactions ON Patrons.PatronID = Transactions.PatronID;
+  
+ 
+ 
+ 
 --
 -- insert/delete/update statements to test the integrity constraints (note: just test the 4 ICs in the project proposal)
 -- all of the following should be rejected
@@ -340,9 +344,8 @@ INSERT INTO Staff VALUES (740, 'Dan Armon', NULL, 4);
 INSERT INTO Transactions VALUES (111, 00111, DATE '2016-12-08', DATE '2016-12-22', DATE '2017-01-06', NULL, 120.99);
 -- Test tIC2
 INSERT INTO Transactions VALUES (111, 01000, DATE '2016-12-08', DATE '2017-01-06', DATE '2017-01-06', NULL, NULL);
-
+ 
 COMMIT;
 --
 SPOOL OFF
-
-
+ 
